81dc9d79a987019dbe2f132a1ce12d702515c945
Justin Fletcher <justinr.fletcher@gmail.com>
2017-10-15 04:42:46 -1000

    Prevented div0 in temp funcs.

--- temperature_functions.py
+++ temperature_functions.py
@@ -4,10 +4,10 @@ import numpy as np
 # CSA temperature function.
 def csa_temperature(i, init_temp):
 
-        return(init_temp / np.log(i))
+        return(init_temp / np.log((i + 1)))
 
 
 # FSA temperature function.
 def fsa_temperature(i, init_temp):
 
-        return(init_temp / i)
+        return(init_temp / (i + 1))
